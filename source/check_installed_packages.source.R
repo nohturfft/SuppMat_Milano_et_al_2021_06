12a031d1ffa0b50821a45f46a016ee1cf8a66cbb
nohturfft <nohturfft@gmail.com>
2021-06-20 08:48:24 +0100

    First commit

---
+++ source/check_installed_packages.source.R
@@ -0,0 +1,49 @@
+installed_packages <- rownames(utils::installed.packages())
+required_cran_packages <- c(
+  "rmarkdown",
+  "knitr",
+  "BiocManager",
+  "magrittr",
+  "dplyr",
+  "details",
+  "rstudioapi",
+  "pryr",
+  "DT",
+  "RColorBrewer",
+  "scales",
+  "devtools",
+  "tibble",
+  "gridExtra",
+  "data.table",
+  "openxlsx",
+  "enrichR",
+  "gplots",
+  "Hmisc",
+  "superheat",
+  "plotly"
+)
+
+required_bioc_packages <- c(
+  "BiocGenerics",
+  "Biobase",
+  "IRanges",
+  "S4Vectors",
+  "AnnotationDbi",
+  "affy",
+  "mouse430a2.db"
+)
+
+for (cran_pkg in required_cran_packages) {
+  if (!cran_pkg %in% installed_packages) {
+    message(paste("Installing:", cran_pkg))
+    install.packages(cran_pkg, dependencies=TRUE)
+  }
+}
+
+for (bioc_pkg in required_bioc_packages) {
+  if (!bioc_pkg %in% installed_packages) {
+    BiocManager::install(bioc_pkg, dependencies=TRUE)
+  }
+}
+
+rm(installed_packages)
